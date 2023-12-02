bd7a8002e9e257782744daa9a8aea122ce29346f
allencarne <allencarne@yahoo.com>
2023-12-02 10:53:46 -0800

    UI Buttons and Script
    Added a few buttons for Soccer Menu UI.
    
    Added some logic to hide and display some buttons based on what is being selected currently to have a more simple and clean look

--- Assets/_Scripts/Menu/SoccerMenu.cs
+++ Assets/_Scripts/Menu/SoccerMenu.cs
@@ -2,9 +2,82 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class SoccerMenu : MonoBehaviour
 {
+    [Header("Sides")]
+    [SerializeField] GameObject blueSide;
+    [SerializeField] GameObject redSide;
+
+    [Header("Blue 1")]
+    [SerializeField] GameObject blue1;
+    [SerializeField] TextMeshProUGUI blue1Text;
+    [SerializeField] Button blue1Left;
+    [SerializeField] Button blue1Right;
+
+    [Header("Red 1")]
+    [SerializeField] GameObject red1;
+    [SerializeField] TextMeshProUGUI red1Text;
+    [SerializeField] Button red1Left;
+    [SerializeField] Button red1Right;
+
+    [Header("Blue 2")]
+    [SerializeField] GameObject blue2;
+    [SerializeField] TextMeshProUGUI blue2Text;
+    [SerializeField] Button blue2Left;
+    [SerializeField] Button blue2Right;
+
+    [Header("Red 2")]
+    [SerializeField] GameObject red2;
+    [SerializeField] TextMeshProUGUI red2Text;
+    [SerializeField] Button red2Left;
+    [SerializeField] Button red2Right;
+
+    [Header("Blue 3")]
+    [SerializeField] GameObject blue3;
+    [SerializeField] TextMeshProUGUI blue3Text;
+    [SerializeField] Button blue3Left;
+    [SerializeField] Button blue3Right;
+
+    [Header("Red 3")]
+    [SerializeField] GameObject red3;
+    [SerializeField] TextMeshProUGUI red3Text;
+    [SerializeField] Button red3Left;
+    [SerializeField] Button red3Right;
+
+    [SerializeField] GameObject playButton;
+
+    enum PlayerType 
+    {
+        None,
+        AI,
+        Player1,
+        Player2,
+        Player3,
+        Player4,
+        Player5,
+        Player6,
+    }
+
+    private void Start()
+    {
+        blueSide.SetActive(false);
+        redSide.SetActive(false);
+
+        blue1.SetActive(false);
+        red1.SetActive(false);
+
+        blue2.SetActive(false);
+        red2.SetActive(false);
+
+        blue3.SetActive(false);
+        red3.SetActive(false);
+
+        playButton.SetActive(false);
+    }
+
     public void FreePlayButton()
     {
         // Tell the GameManager that FreePlay is Selected
@@ -23,28 +96,49 @@ public class SoccerMenu : MonoBehaviour
 
     public void OneVsOneButton()
     {
-        // Game Mode
+        // Buttons
+        blueSide.SetActive(true);
+        redSide.SetActive(true);
 
-        // Players
+        blue1.SetActive(true);
+        red1.SetActive(true);
 
-        SceneManager.LoadScene("Soccer");
+        blue2.SetActive(false);
+        red2.SetActive(false);
+
+        blue3.SetActive(false);
+        red3.SetActive(false);
     }
 
     public void TwoVsTwoButton()
     {
-        // Game Mode
+        // Buttons
+        blueSide.SetActive(true);
+        redSide.SetActive(true);
 
-        // Players
+        blue1.SetActive(true);
+        red1.SetActive(true);
 
-        SceneManager.LoadScene("Soccer");
+        blue2.SetActive(true);
+        red2.SetActive(true);
+
+        blue3.SetActive(false);
+        red3.SetActive(false);
     }
 
     public void ThreeVsThreeButton()
     {
-        // Game Mode
+        // Buttons
+        blueSide.SetActive(true);
+        redSide.SetActive(true);
 
-        // Players
+        blue1.SetActive(true);
+        red1.SetActive(true);
 
-        SceneManager.LoadScene("Soccer");
+        blue2.SetActive(true);
+        red2.SetActive(true);
+
+        blue3.SetActive(true);
+        red3.SetActive(true);
     }
 }
