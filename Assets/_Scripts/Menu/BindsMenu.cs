1ea35b3136e64adbf3aaec9225a451cb991f75f6
allencarne <allencarne@yahoo.com>
2023-12-10 22:08:05 -0800

    Reset Binds for Keyboard working

--- Assets/_Scripts/Menu/BindsMenu.cs
+++ Assets/_Scripts/Menu/BindsMenu.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System;
 
 public class BindsMenu : MonoBehaviour
 {
@@ -26,8 +30,17 @@ public class BindsMenu : MonoBehaviour
     [SerializeField] TextMeshProUGUI c_PauseCurrentBind;
     [SerializeField] TextMeshProUGUI c_ResetCurrentBind;
 
+    // Input
     [SerializeField] InputActionAsset asset;
+    [SerializeField] InputActionReference SteerAction;
+    [SerializeField] InputActionReference AccelerateAction;
+    [SerializeField] InputActionReference BrakeAction;
+    [SerializeField] InputActionReference BoostAction;
+    [SerializeField] InputActionReference DriftAction;
+    [SerializeField] InputActionReference PauseAction;
+    [SerializeField] InputActionReference ResetAction;
 
+    InputActionRebindingExtensions.RebindingOperation rebindingOperation;
 
     private void Start()
     {
@@ -69,24 +82,232 @@ public class BindsMenu : MonoBehaviour
 
     public void K_SteerLeftRebind()
     {
+        // DeSelect the currently Selected UI Button
+        EventSystem.current.SetSelectedGameObject(null);
 
+        // Disable before Re-Bind
+        SteerAction.action.Disable();
+
+        // Update Button Text to say "Press Any Button"
+        k_SteerLeftCurrentBind.text = "Press Any Button";
+
+
+        rebindingOperation = SteerAction.action.PerformInteractiveRebinding()
+            .WithTargetBinding(2)
+            .WithControlsExcluding("<Gamepad>")
+            .OnMatchWaitForAnother(0.1f)
+            .OnComplete(operation =>
+            {
+                k_SteerLeftCurrentBind.text = InputControlPath.ToHumanReadableString(SteerAction.action.bindings[2].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice);
+
+                rebindingOperation.Dispose();
+                SteerAction.action.Enable();
+            })
+            .Start();
     }
 
     public void K_SteerRightRebind()
     {
+        // DeSelect the currently Selected UI Button
+        EventSystem.current.SetSelectedGameObject(null);
 
+        // Disable before Re-Bind
+        SteerAction.action.Disable();
+
+        // Update Text to say "Press Any Button"
+        k_SteerRightCurrentBind.text = "Press Any Button";
+
+        rebindingOperation = SteerAction.action.PerformInteractiveRebinding()
+            .WithTargetBinding(3)
+            .WithControlsExcluding("<Gamepad>")
+            .OnMatchWaitForAnother(0.1f)
+            .OnComplete(operation =>
+            {
+                // Assuming the right steering is at index 3
+                k_SteerRightCurrentBind.text = InputControlPath.ToHumanReadableString(SteerAction.action.bindings[3].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice);
+
+                rebindingOperation.Dispose();
+                SteerAction.action.Enable();
+            })
+            .Start();
     }
 
     public void K_AccelerateRebind()
     {
+        // DeSelect the currently Selected UI Button
+        EventSystem.current.SetSelectedGameObject(null);
 
+        // Disable before Re-Bind
+        AccelerateAction.action.Disable();
+
+        // Update Button Text to say "Press Any Button"
+        k_AccelerateCurrentBind.text = "Press Any Button";
+
+
+        rebindingOperation = AccelerateAction.action.PerformInteractiveRebinding()
+            .WithControlsExcluding("<Gamepad>")
+            .OnMatchWaitForAnother(0.1f)
+            .OnComplete(operation =>
+            {
+                k_AccelerateCurrentBind.text = InputControlPath.ToHumanReadableString(AccelerateAction.action.bindings[0].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice);
+
+                rebindingOperation.Dispose();
+                AccelerateAction.action.Enable();
+            })
+            .Start();
+    }
+
+    public void K_BrakeRebind()
+    {
+        // DeSelect the currently Selected UI Button
+        EventSystem.current.SetSelectedGameObject(null);
+
+        // Disable before Re-Bind
+        BrakeAction.action.Disable();
+
+        // Update Button Text to say "Press Any Button"
+        k_BrakeCurrentBind.text = "Press Any Button";
+
+
+        rebindingOperation = BrakeAction.action.PerformInteractiveRebinding()
+            .WithControlsExcluding("<Gamepad>")
+            .OnMatchWaitForAnother(0.1f)
+            .OnComplete(operation =>
+            {
+                k_BrakeCurrentBind.text = InputControlPath.ToHumanReadableString(BrakeAction.action.bindings[1].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice);
+
+                rebindingOperation.Dispose();
+                BrakeAction.action.Enable();
+            })
+            .Start();
+    }
+
+    public void K_BoostRebind()
+    {
+        // DeSelect the currently Selected UI Button
+        EventSystem.current.SetSelectedGameObject(null);
+
+        // Disable before Re-Bind
+        BoostAction.action.Disable();
+
+        // Update Button Text to say "Press Any Button"
+        k_BoostCurrentBind.text = "Press Any Button";
+
+
+        rebindingOperation = BoostAction.action.PerformInteractiveRebinding()
+            .WithControlsExcluding("<Gamepad>")
+            .OnMatchWaitForAnother(0.1f)
+            .OnComplete(operation =>
+            {
+                k_BoostCurrentBind.text = InputControlPath.ToHumanReadableString(BoostAction.action.bindings[1].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice);
+
+                rebindingOperation.Dispose();
+                BoostAction.action.Enable();
+            })
+            .Start();
+    }
+
+    public void K_DriftRebind()
+    {
+        // DeSelect the currently Selected UI Button
+        EventSystem.current.SetSelectedGameObject(null);
+
+        // Disable before Re-Bind
+        DriftAction.action.Disable();
+
+        // Update Button Text to say "Press Any Button"
+        k_DriftCurrentBind.text = "Press Any Button";
+
+
+        rebindingOperation = DriftAction.action.PerformInteractiveRebinding()
+            .WithControlsExcluding("<Gamepad>")
+            .OnMatchWaitForAnother(0.1f)
+            .OnComplete(operation =>
+            {
+                k_DriftCurrentBind.text = InputControlPath.ToHumanReadableString(DriftAction.action.bindings[1].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice);
+
+                rebindingOperation.Dispose();
+                DriftAction.action.Enable();
+            })
+            .Start();
+    }
+
+    public void K_PauseRebind()
+    {
+        // DeSelect the currently Selected UI Button
+        EventSystem.current.SetSelectedGameObject(null);
+
+        // Disable before Re-Bind
+        PauseAction.action.Disable();
+
+        // Update Button Text to say "Press Any Button"
+        k_PauseCurrentBind.text = "Press Any Button";
+
+
+        rebindingOperation = PauseAction.action.PerformInteractiveRebinding()
+            .WithControlsExcluding("<Gamepad>")
+            .OnMatchWaitForAnother(0.1f)
+            .OnComplete(operation =>
+            {
+                k_PauseCurrentBind.text = InputControlPath.ToHumanReadableString(PauseAction.action.bindings[1].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice);
+
+                rebindingOperation.Dispose();
+                PauseAction.action.Enable();
+            })
+            .Start();
+    }
+
+    public void K_ResetRebind()
+    {
+        // DeSelect the currently Selected UI Button
+        EventSystem.current.SetSelectedGameObject(null);
+
+        // Disable before Re-Bind
+        ResetAction.action.Disable();
+
+        // Update Button Text to say "Press Any Button"
+        k_ResetCurrentBind.text = "Press Any Button";
+
+
+        rebindingOperation = ResetAction.action.PerformInteractiveRebinding()
+            .WithControlsExcluding("<Gamepad>")
+            .OnMatchWaitForAnother(0.1f)
+            .OnComplete(operation =>
+            {
+                k_ResetCurrentBind.text = InputControlPath.ToHumanReadableString(ResetAction.action.bindings[1].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice);
+
+                rebindingOperation.Dispose();
+                ResetAction.action.Enable();
+            })
+            .Start();
     }
 
     // Controller
 
     public void C_SteerLeftRebind()
     {
+        // DeSelect the currently Selected UI Button
+        EventSystem.current.SetSelectedGameObject(null);
 
+        // Disable before Re-Bind
+        SteerAction.action.Disable();
+
+        // Update Button Text to say "Press Any Button"
+        c_SteerLeftCurrentBind.text = "Press Any Button";
+
+        rebindingOperation = SteerAction.action.PerformInteractiveRebinding()
+            .WithControlsExcluding("<Mouse>")
+            .WithControlsExcluding("<Mouse>/LeftButton")
+            .WithControlsExcluding("Keyboard")
+            .OnMatchWaitForAnother(0.1f)
+            .OnComplete(operation =>
+            {
+                c_SteerLeftCurrentBind.text = InputControlPath.ToHumanReadableString(SteerAction.action.bindings[0].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice);
+
+                rebindingOperation.Dispose();
+                SteerAction.action.Enable();
+            })
+            .Start();
     }
 
     public void C_SteerRightRebind()
@@ -96,6 +317,32 @@ public class BindsMenu : MonoBehaviour
 
     public void C_AccelerateRebind()
     {
+        // DeSelect the currently Selected UI Button
+        EventSystem.current.SetSelectedGameObject(null);
 
+        // Disable before Re-Bind
+        AccelerateAction.action.Disable();
+
+        // Update Button Text to say "Press Any Button"
+        c_AccelerateCurrentBind.text = "Press Any Button";
+
+        rebindingOperation = AccelerateAction.action.PerformInteractiveRebinding()
+            .WithControlsExcluding("<Mouse>")
+            .WithControlsExcluding("<Mouse>/LeftButton")
+            .WithControlsExcluding("Keyboard")
+            .OnMatchWaitForAnother(0.1f)
+            .OnComplete(operation =>
+            {
+                c_AccelerateCurrentBind.text = InputControlPath.ToHumanReadableString(AccelerateAction.action.bindings[0].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice);
+
+                rebindingOperation.Dispose();
+                AccelerateAction.action.Enable();
+            })
+            .Start();
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
