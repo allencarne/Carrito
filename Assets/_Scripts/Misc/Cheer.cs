10fbf25cdfc2c955ece8bac974a57cdd9de0bb98
allencarne <allencarne@yahoo.com>
2024-01-13 09:35:01 -0800

    Crowd Added + Celebration

---
+++ Assets/_Scripts/Misc/Cheer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheer : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] GameObject cheerParticle;
+
+    private void OnEnable()
+    {
+        Ball.OnScored += StartCheer;
+    }
+
+    private void OnDisable()
+    {
+        Ball.OnScored -= StartCheer;
+    }
+
+    void StartCheer()
+    {
+        animator.Play("Cheer");
+
+        Instantiate(cheerParticle, transform.position, transform.rotation);
+    }
+}
